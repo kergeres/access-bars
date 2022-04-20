241bc3e6e91701929bfb3e44a129299494f37526
kergeres <55786735+kergeres@users.noreply.github.com>
2022-04-20 23:21:36 +0200

    b4 eject

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Header from './components/header/Header';
 import Appointment from './components/header/appointment/Appointment';
 import Contact from './components/header/contact/Contact';
 import AboutUs from './components/header/aboutUs/AboutUs';
+import Landing from './components/header/landing/Landing';
 
 function App() {
   return (
 
     <div className="App">
+
       <Header />
+      <Landing />
       <AboutUs />
       <Appointment />
       <Contact />
---
+++ src/components/header/landing/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Landing = () => {
+
+    return (
+        <h3>land</h3>
+    )
+}
+
+export default Landing
