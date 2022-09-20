558e4bf58c6820e96c370f2b4330c0c458a0c299
Davipp <pappdavidlevele@gmail.com>
2022-09-20 21:18:30 +0200

    text from rka

---
+++ src/components/header/aboutUs/AboutUs.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import classes from "./AboutUs.css";
+const AboutUs = () => {
+  return (
+    <section id="aboutUs">
+      <div className="infoContainer">
+        <h2>Mi az Access Bars?</h2>
+        <hr></hr>
+        <p>
+          Az életünk nagyrészét különböző tudatalatti folyamatok irányítják,
+          amire mi egyáltalán nem vagyunk hatással. Ezek a berögzült
+          mechanizmusok, amiket leginkább gyerekkorunk történései, illetve a
+          világ működése alakít ki bennünk, sokszor elképesztően mérgező
+          hatással vannak a hétköznapi döntéseinkre. Az egészséges életnek nem
+          csupán fizikai tényezői vannak. Fontos, hogy a lelki és szellemi
+          kiegyensúlyozottságra is ugyanakkora figyelmet fordítsunk, mint az
+          étkezésre vagy a sportolásra. Erre kínál egy alternatív lehetőséget
+          egy teljesen új kezelési forma, az Access Bars. A fejünkön összesen 32
+          Bar pont található, amik körkörösen futnak a koponyánkon, mindegyik az
+          életünk különböző területeihez kapcsolható. A nagyjából egy órás
+          kezelés alatt ezeket a pontokat stimulálják, aminek hatására a
+          hétköznapjainkat átszövő blokkok feloldódnak. Ennek hatására képesek
+          leszünk új gondolatokat befogadni, eddig nem látott lehetőségek
+          tárulnak majd elénk. Az eddig domináló hitrendszereket félre tudjuk
+          majd tenni, ezzel pedig javulni fog a közérzetünk. A negatív érzések
+          és gondolatok kikerülnek az életünkből, ennek következtében pedig
+          lehetőség nyílik egy sokkal tudatosabb létforma kialakítására. A
+          kezelést egy kisebb konzultáció előzi meg, hogy a kezelő tudja, melyik
+          pontokra kell fókuszálni, honnan kell a legtöbb blokkot kioldani. Az
+          eredmény pedig egy sokkal kiegyensúlyozottabb, stresszmentesebb élet
+          lesz, hiszen lehetőséget kaphatunk arra, hogy egy teljesen új
+          perspektívából szemlélődhessünk. Az Access Bars kezelés egy kiváló
+          alkalom ahhoz, hogy közelebb kerüljünk egy nyugodt, boldog élethez. A
+          legrosszabb, ami történhet, hogy egy mély relaxáció hatását érezzük és
+          a kezelést követő időszak sokkal nyugodtabban telik majd, mint
+          általában, ezért is éri meg adni neki egy esélyt.
+        </p>
+      </div>
+      <div className="infoContainer">
+        <h2>Hogy néz ki egy kezelés?</h2>
+
+        <hr></hr>
+        <p>
+          A kezelés előtti konzultáció nagyjából 20 percet vesz igénybe, ezalatt
+          a kezelő felméri, pontosan mire is van szükség. Az alkalom további
+          része vízszintes pozícióban zajlik, nagyjából egy órát vesz igénybe.
+          Ezalatt a kezelő a Bars pontokat stimulálja, többnyire a fejen, de
+          alkalmanként a karokon és a lábakon is, egy-egy pontot megközelítőleg
+          10-15 percig, utána megy tovább. Mindez a lehető legnyugodtabb
+          környezetben történik, a háttérben zene szól, igény szerint füstölő is
+          használható. Eközben a kezelt teljesen megnyugszik, meditatív
+          állapotba kerül, sőt van olyan is, aki elalszik, még más inkább
+          beszélgetne közben, mindenre van lehetőség. Természetesen bármikor
+          megszakítható a folyamat, nem kötelező kihasználni az egy órát, a
+          lényeg, hogy jól érezzük magunkat, mindenki saját igénye szerint.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
